238021cfa3a1e646b6cd22f8c3beca1d52b5b4d2
Narek Yazeryan <yazeryann@gmail.com>
2025-02-20 02:51:15 +0400

    [Troubleshoot] Fix the biomejs integration

--- auth/src/controllers/refresh.controller.ts
+++ auth/src/controllers/refresh.controller.ts
@@ -4,25 +4,24 @@ import redis from 'connections/redisClient';
 import type { Request, Response } from 'express';
 
 export default async function refreshController(req: Request, res: Response) {
-  if (!req.body.refreshToken) {
-    res.error('Refresh token is required', 400);
-    return;
-  }
+	if (!req.body.refreshToken) {
+		res.error('Refresh token is required', 400);
+		return;
+	}
 
-  const refreshToken = req.body.refreshToken.trim();
-  const { userId } = verifyRefreshToken(refreshToken);
-  const storedToken = await redis.get(userId);
+	const refreshToken = req.body.refreshToken.trim();
+	const { userId } = verifyRefreshToken(refreshToken);
+	const storedToken = await redis.get(userId);
 
-  if (refreshToken !== storedToken) {
-    res.error('Invalid refresh token', 400);
-    return;
-  }
+	if (refreshToken !== storedToken) {
+		res.error('Invalid refresh token', 400);
+		return;
+	}
 
-  const { accessToken, refreshToken: newRefreshToken } = await initTokens(userId);
+	const { accessToken, refreshToken: newRefreshToken } = await initTokens(userId);
 
-  res.success({
-    accessToken,
-    refreshToken: newRefreshToken,
-  });
-
+	res.success({
+		accessToken,
+		refreshToken: newRefreshToken,
+	});
 }
