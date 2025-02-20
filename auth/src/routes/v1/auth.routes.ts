48b8908dd61f02ea19dc3c0426b5e1a4e5728d18
Narek Yazeryan <yazeryann@gmail.com>
2025-02-20 11:39:09 +0400

    [REFACTORING] Updated the imports style for auth routes

--- auth/src/routes/v1/auth.routes.ts
+++ auth/src/routes/v1/auth.routes.ts
@@ -1,10 +1,12 @@
+import express from 'express';
+
 import validateCredentials from '@middlewares/validateCredentials.middleware';
 import validateRefreshToken from '@middlewares/validateRefreshToken.middleware';
-import loginController from 'controllers/login.controller';
-import logoutController from 'controllers/logout.controller';
-import refreshController from 'controllers/refresh.controller';
-import registerController from 'controllers/register.controller';
-import express from 'express';
+
+import loginController from '@controllers/login.controller';
+import logoutController from '@controllers/logout.controller';
+import refreshController from '@controllers/refresh.controller';
+import registerController from '@controllers/register.controller';
 
 const authRouter = express.Router();
 
